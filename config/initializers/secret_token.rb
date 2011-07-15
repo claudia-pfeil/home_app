420d4aaae89e08f446080df67387230de3711cc6
Claudia Pfeil <cleo_sunshine@yahoo.de>
2011-07-15 14:55:22 +0200

    my home_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeApp::Application.config.secret_token = '450f802fb06d3b182c84abc09f5f0c6aa940ba6995db80e15abddb870319959ad9cd7f0e5990f836d27d997a8f241e84b0a24d9646bd734ee08da7e6ce8a9a6b'
